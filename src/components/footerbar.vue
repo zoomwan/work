0b6a0ac95c4e14dfdd1df0c960697866ab1e880e
zoomwan <hqiang_183@163.com>
2021-06-05 15:02:17 +0800

    'tabbar完成'

---
+++ src/components/footerbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer">
+    <van-tabbar route>
+      <van-tabbar-item replace to="/home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item replace to="/search" icon="apps-o">分类</van-tabbar-item>
+      <van-tabbar-item replace to="/home" icon="search">发现</van-tabbar-item>
+      <van-tabbar-item replace to="/search" icon="cart-o"
+        >购物车</van-tabbar-item
+      >
+      <van-tabbar-item replace to="/home" icon="friends-o"
+        >我的</van-tabbar-item
+      >
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/headerbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <div class="fan" @click="back"><span>返回</span> <slot></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+
+  methods: {},
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+};
+</script>
+<style scoped>
+.header {
+  position: fixed;
+  z-index: 3;
+  top: 0;
+  left: 0;
+  height: 40px;
+  line-height: 40px;
+  background: red;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0 10px;
+  width: 100%;
+}
+</style>
